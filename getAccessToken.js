d535d1d29e72d4717d921d98afcb3f16f1a860b8
FlorentP <florent.payet@epitech.eu>
2024-02-02 10:26:48 +0100

    Clean Code

--- getAccessToken.js
+++ getAccessToken.js
@@ -1,22 +1,17 @@
-
 import axios from 'axios'
 import 'dotenv/config';
 const client_id = process.env.CLIENT_ID
 const client_secret = process.env.CLIENT_SECRET
 const scope = process.env.SCOPE
 
-
-
 export const getAccessToken = async () => {
   try {
     const response = await axios.post(
       `https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire&grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}&scope=${scope}`
     );
-
     const date = new Date();
     date.setSeconds(date.getSeconds() + response.data.expires_in);
     const data = { token: response.data.access_token, expires_in: date.getTime() };
-
     return data;
   } catch (error) {
     if (error.response) {
@@ -24,7 +19,5 @@ export const getAccessToken = async () => {
     } else {
       console.log('Error', error.message);
     }
-
-    throw error; // You may want to handle the error appropriately or rethrow it.
   }
 };
